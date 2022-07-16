2cde2f9f92c6c0e294f903777f74786e10424df2
NuXareon <millas@gmail.com>
2022-07-16 11:47:41 +0200

    Dice consumption

--- RollOfTheDice/Assets/Scripts/DiceComponent.cs
+++ RollOfTheDice/Assets/Scripts/DiceComponent.cs
@@ -19,11 +19,7 @@ public class DiceComponent : MonoBehaviour
         manager = gameLogic.GetComponent<DiceManager>();
 
         UI = Instantiate(manager.diceUIPrefab, UILayer.transform);
-        RectTransform UITransform = UI.GetComponent<RectTransform>();
         // #OverrideSorting?
-
-        Vector2 UIPosition = new Vector2(50 + ((float)id * (125.0f + 5.0f)), -55);
-        UITransform.anchoredPosition = UIPosition;
 
         UpdateUI();
     }
@@ -39,6 +35,11 @@ public class DiceComponent : MonoBehaviour
     {
     }
 
+    void OnDestroy()
+    {
+        Destroy(UI);
+    }
+
     IEnumerator RollDice()
     {
         yield return new WaitForSecondsRealtime(manager.rollDiceTimeSeconds);
@@ -47,8 +48,13 @@ public class DiceComponent : MonoBehaviour
         UpdateUI();
     }
 
-    void UpdateUI()
+    public void UpdateUI()
     {
+        // #Improve pos calculation
+        RectTransform UITransform = UI.GetComponent<RectTransform>();
+        Vector2 UIPosition = new Vector2(50 + ((float)id * (125.0f + 5.0f)), -55);
+        UITransform.anchoredPosition = UIPosition;
+
         TMP_Text UIText = UI.GetComponentInChildren<TMP_Text>();
         UIText.text = value.ToString();
     }
--- RollOfTheDice/Assets/Scripts/DiceManager.cs
+++ RollOfTheDice/Assets/Scripts/DiceManager.cs
@@ -7,22 +7,23 @@ public class DiceManager : MonoBehaviour
     public float rollDiceTimeSeconds = 10.0f;
     public int numDice = 1;
     public GameObject diceUIPrefab;
+    public GameObject diceUIHighlightPrefab;
 
     public List<GameObject> dice = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
+        // #make this better
+        GameObject UILayer = GameObject.FindGameObjectWithTag("UI");
+        GameObject UIHighlihgt = Instantiate(diceUIHighlightPrefab, UILayer.transform);
+        RectTransform UITransform = UIHighlihgt.GetComponent<RectTransform>();
+        Vector2 UIPosition = new Vector2(50, -55);
+        UITransform.anchoredPosition = UIPosition;
+
         for (int i = 0; i < numDice; i++)
         {
-            GameObject newDice = new GameObject();
-            newDice.SetActive(false);
-            newDice.transform.parent = transform;
-            newDice.name = "Dice" + i;
-            DiceComponent newDiceComponent = newDice.AddComponent<DiceComponent>();
-            newDiceComponent.id = i;
-            newDice.SetActive(true);
-
+            GameObject newDice = CreateDice(i);
             dice.Add(newDice);
         }
     }
@@ -30,6 +31,63 @@ public class DiceManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // #TEMP
+        if (Input.GetButtonDown("Jump"))
+        {
+            ConsumeDice();
+        }
+    }
+
+    GameObject CreateDice(int id)
+    {
+        GameObject newDice = new GameObject();
+        newDice.SetActive(false);
+        newDice.transform.parent = transform;
+        newDice.name = "Dice" + id;
+        DiceComponent newDiceComponent = newDice.AddComponent<DiceComponent>();
+        newDiceComponent.id = id;
+        newDice.SetActive(true);
+
+        return newDice;
+    }
+
+    // Returns dice value and removed dice from queue
+    // 0 means no dice available
+    public int ConsumeDice()
+    {
+        if (dice.Count == 0)
+        {
+            return 0;
+        }
+
+        GameObject currentDice = dice[0];
+        DiceComponent diceComp = currentDice.GetComponent<DiceComponent>();
+        int value = diceComp.value;
+
+        if (value != 0)
+        {
+            dice.RemoveAt(0);
+            Destroy(currentDice);
+            FillDiceQueue();
+        }
+
+        return value;
+    }
+
+    void FillDiceQueue()
+    {
+        int i = 0;
+        for (; i < dice.Count; i++)
+        {
+            dice[i].name = "Dice" + i;
+            DiceComponent newDiceComponent = dice[i].GetComponent<DiceComponent>();
+            newDiceComponent.id = i;
+            newDiceComponent.UpdateUI();
+        }
+        for (; i < numDice; i++)
+        {
+            GameObject newDice = CreateDice(i);
+            dice.Insert(i, newDice);
+        }
     }
 }
